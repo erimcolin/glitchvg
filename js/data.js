b0e762716de5d513d27235ecd842c5ddc3cbf9ed
Samahara <le.chat.au.chapeau@hotmail.com>
2018-02-06 17:23:04 -0600

    data

---
+++ js/data.js
@@ -0,0 +1,47 @@
+var videogames = [
+  {
+    'company': 'Nintendo',
+    'genre': 'Racing',
+    'metacritic': '92',
+    'name': 'Mario Kart 8 Deluxe',
+    'platform': 'Nintendo Switch',
+    'rating': 'E',
+    'released-date': 'Apr 28, 2017',
+  },
+  {
+    'company': 'Konami'
+    'genre': 'Action Adventure, Open-World',
+    'metacritic': '93',
+    'name': 'Metal Gear Solid V',
+    'platform': 'PC, PS3, PS4, XBOX 360, XBOX ONE',
+    'rating': 'M',
+    'released-date': 'Sep 1, 2015',
+  },
+  {
+    'company': 'Square Enix',
+    'genre': 'Action RPG',
+    'metacritic': '81',
+    'name': 'Final Fantasy XV',
+    'platform': 'PS4',
+    'rating': 'T',
+    'released-date': 'Nov 29, 2016',
+  },
+  {
+    'company': 'Studio MDHR',
+    'genre': 'Action, platformer, 2D',
+    'metacritic': '88',
+    'name': 'CUPHEAD',
+    'platform': 'PC, XBOX ONE',
+    'rating': 'E10+',
+    'released-date': 'sep 29, 2017',
+  },
+  {
+    'company': 'Square Enix',
+    'genre': 'Action Adventure, Linear',
+    'metacritic': '88',
+    'name': 'Rise of the Tomb Raider: 20 Year Celebration',
+    'platform': 'PS4, PC, XBOX ONE',
+    'rating': 'M',
+    'released-date': 'Oct 11, 2016',
+  }
+];
